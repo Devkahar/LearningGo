fd2269154f15abd4592f2365e96084621e8090cd
DEV KAHAR <dev@atliq.com>
2023-05-26 00:32:39 +0530

    15methods

---
+++ 15methods/main.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Learning Methods in Go")
+	// no inh eritance in golang; No super or parent
+	dev := User{"Dev", "dev@gmail.com", true, 20}
+	fmt.Printf("Dev Details %+v", dev)
+	dev.GetStatus()
+	dev.NewEmail()
+}
+
+type User struct {
+	Name   string
+	Email  string
+	Status bool
+	Age    int
+}
+
+func (u User) GetStatus() bool {
+	fmt.Println("User status ", u.Status)
+	return u.Status
+}
+
+func (u User) NewEmail() {
+	u.Email = "dev@go.dev"
+	fmt.Println("User Email Address ", u.Email)
+}
