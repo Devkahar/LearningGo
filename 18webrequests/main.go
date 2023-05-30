916b23a2c423875ce6399b2b15bdc86467a1318b
DEV KAHAR <dev@atliq.com>
2023-05-31 00:13:01 +0530

    18webrequests

---
+++ 18webrequests/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+const url = "http://loc.dev"
+
+func checkNilErr(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+func main() {
+	fmt.Println("LOC Web Request")
+
+	response, err := http.Get(url)
+
+	checkNilErr(err)
+	fmt.Printf("Response is of type %T\n", response)
+	defer response.Body.Close() // callers responsibility to close connection
+
+	databytes, err := ioutil.ReadAll(response.Body)
+
+	checkNilErr(err)
+
+	content := string(databytes)
+
+	fmt.Println(content)
+
+}
