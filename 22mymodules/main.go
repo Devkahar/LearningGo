501aeaa783760d374835da170b1467020c40a65e
Dev <Devkahar>
2023-06-01 17:18:50 +0530

    22mymodules

---
+++ 22mymodules/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	fmt.Println("Learning about go mod")
+	r := mux.NewRouter()
+	r.HandleFunc("/", serveHome).Methods("GET")
+	log.Fatal(http.ListenAndServe(":4000", r))
+}
+
+func serveHome(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("<h1>Learning golang from youtube</h1>"))
+}
